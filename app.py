ccc4d1bf6e2973f646f909cbf38acd4df288498e
5ponsky <mailgd9@gmail.com>
2017-09-22 22:26:24 -0500

    changed html message

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <p>It is currently</p>
 
-    
+    """
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
