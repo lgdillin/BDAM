9ed5a12740256e877aade5b19678757c2db4c13b
5ponsky <mailgd9@gmail.com>
2017-09-22 22:17:43 -0500

    added app files for Flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
