788b10dcb70bfb67f2b3425b6bde81ada5e5ce99
Liam Dillingham <mailgd9@gmail.com>
2017-10-10 15:55:23 -0500

    working on querying

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import reader
+
 from flask import Flask, render_template
 from datetime import datetime
 app = Flask(__name__)
@@ -8,9 +10,10 @@ def homepage():
     return render_template('index.html')
     # return """<h1>Hello heroku</h1><p>It is currently</p>"""
 
-@app.route('/query')
+@app.route('/query/')
 def query():
-    return 'click'
+    # return pprint.pprint(reader.query())
+    return query()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
