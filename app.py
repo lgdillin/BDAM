e2fd2d9e94b7af8ecc8a1d669d4f54969def3d1d
Liam Dillingham <mailgd9@gmail.com>
2017-10-12 16:07:53 -0500

    adjusted ability to query

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import reader
+import reader as reader
+import analyze as analyze
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from datetime import datetime
 app = Flask(__name__)
 
@@ -11,9 +12,49 @@ def homepage():
     # return """<h1>Hello heroku</h1><p>It is currently</p>"""
 
 @app.route('/query/')
-def query():
+def returnRecord():
     # return pprint.pprint(reader.query())
-    return query()
+    return reader.query()
+
+@app.route('/populate/')
+def populate():
+    reader.populate()
+    return
+
+@app.route('/analyze')
+def analyze():
+    return analysis()
+
+# Used for drawing lexical graphs
+@app.route("/simple.png")
+def simple():
+    import datetime
+    import StringIO
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- reader.py
+++ reader.py
@@ -1,19 +1,39 @@
-import pprint
+import pprint, json
 from pymongo import MongoClient
 
+def populate():
+    client = MongoClient('mongodb://lgdillin:Big.Data-1@bdam-shard-00-00-awflg.mongodb.net:27017,bdam-shard-00-01-awflg.mongodb.net:27017,bdam-shard-00-02-awflg.mongodb.net:27017/test?ssl=true&replicaSet=bdam-shard-0&authSource=admin', 27017)
+    db = client['MONGODB']
+    collection = db['MONGODB']
+
+    #Populate with some values
+    post = {"content":"One day I woke up and took a shit and then went to walmart. I saw a giant bread toaster"}
+    post_id = collection.insert(post)
+    post = {"content":"Yesterday on the news a brown person are abused by the police"}
+    post_id = collection.insert(post)
+    post = {"content":"Words in a string are useful sometimes, the context is irrelevant"}
+    return
+
+# Query the server
 def query():
     client = MongoClient('mongodb://lgdillin:Big.Data-1@bdam-shard-00-00-awflg.mongodb.net:27017,bdam-shard-00-01-awflg.mongodb.net:27017,bdam-shard-00-02-awflg.mongodb.net:27017/test?ssl=true&replicaSet=bdam-shard-0&authSource=admin', 27017)
     db = client['MONGODB']
+    collection = db['MONGODB']
 
-    post = {"author":"liam"}
-    collection = db.MONGODB
-    post_id = collection.insert_one(post).inserted_id
+    result = ""
+    for post in collection.find():
+        result = result + pprint.pformat(post)
+
+    return result
+    #print(json.loads(result)["content"])
+    #return food["content"]
+    #return pprint.pformat(collection.count())
+    #return pprint.pformat(collection.find())
+    #for post in collection.find():
+
     #pprint.pprint(collection.find_one())
-    return collection.find_one()
+    #return pprint.pformat(collection.find_one())
+    #return pprint.pformat(db.collection_names())
 
-# document = client.db.collection.find_one({'author':'liam'})
-'''
-collection = db.MONGODB
-for i in post:
-    pprint.pprint(i)
-'''
+def test():
+    return "works"
