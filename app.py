4ba0138af48ad166a300181c16800e9b2d86d40d
Liam Dillingham <mailgd9@gmail.com>
2017-10-23 14:09:30 -0500

    heroku

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def search(hashtag):
 def results():
     hashtag = "sport"
     data = reader.topFive(hashtag)
-    return render_template('querypage.html', data=data)
+    return data
+    #return render_template('querypage.html', data=data)
 
 @app.route('/query/')
 def query():
