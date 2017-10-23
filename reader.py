a395c27e75109b02908ab9895c06d0b15dcc0993
Liam Dillingham <mailgd9@gmail.com>
2017-10-23 15:59:21 -0500

    heroku

--- reader.py
+++ reader.py
@@ -20,9 +20,9 @@ def mostActiveTweeters(hashtag):
     collection = db['twitter_{0}'.format(hashtag)]
     pipeline = [ {"$unwind": "$user.screen_name"}, {"$group": {"_id": "$user.screen_name", "count": {"$sum": 1}}}, {"$sort": SON([("count", -1), ("_id", -1)])} ]
     #return json.dumps(list(collection.aggregate(pipeline))).encode('ascii', 'ignore')
-    json = json.loads(json.dumps(list(collection.aggregate(pipeline))))
+    jsonOut = json.loads(json.dumps(list(collection.aggregate(pipeline))))
     data = {}
-    for doc in json:
+    for doc in jsonOut:
         data.update({doc['_id']:doc['count']})
     return data
     #return json.dumps(list(collection.aggregate(pipeline)), ensure_ascii=True).encode('utf8')
