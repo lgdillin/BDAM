f12723ccc2b4a464aff40fa0a4425677d78a0f26
Liam Dillingham <mailgd9@gmail.com>
2017-10-24 15:28:22 -0500

    heroku

--- reader.py
+++ reader.py
@@ -35,7 +35,7 @@ def getUserTweets(hashtag, screen_name):
     #results = collection.find( {'user.{0}'.format(screen_name) : '{0}'.format(screen_name)}, {'text':1, '_id':0})
     results = collection.find( {'user.screen_name': screen_name}, {'text':1, 'user.screen_name':1, '_id':0} )
 
-    jsonOut = json.loads(results)
+    jsonOut = json.loads(json.dumps(list(results)))
     data = {}
     for doc in results:
         data.update({doc['user']['screen_name']:doc['text']})
